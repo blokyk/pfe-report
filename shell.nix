ac0b7ecef3e01fe68122d86dde2cc675d9171bf3
blokyk <courvoisier.zoe@gmail.com>
2026-08-27 17:43:30 +0200

    chore: use typst-languagetool for spellchecking

--- shell.nix
+++ shell.nix
@@ -3,7 +3,19 @@ let
   pkgs = import pins.nixpkgs {
     overlays = [ (import pins.press) ];
   };
+  zpkgs = pkgs.callPackage (pins.zpkgs + "/pkgs") {};
+
   vscode-ext-hook = pkgs.callPackage pins.vscode-ext-hook.outPath {};
+
+  # until #71 lands, use our version instead
+  typst-languagetool = (zpkgs.typst-languagetool.override {backends = ["server"];}).overrideAttrs {
+    src = pkgs.fetchFromGitHub {
+      owner = "blokyk";
+      repo = "typst-languagetool";
+      rev = "289aa6b1a79034874d80b41bd88ffd53849b2f4f";
+      hash = "sha256-7dHDGMU5qO7WEvj2bNPkZrBVJfwpsOrsMEClZU4QQio=";
+    };
+  };
 
   document = pkgs.buildTypstDocument {
     pname = "report";
@@ -20,6 +32,8 @@ with pkgs;
 mkShell {
   inputsFrom = [ document ];
   packages = [
+    typst-languagetool
+
     vscode-ext-hook
   ];
 
@@ -30,6 +44,7 @@ mkShell {
       ];
 
       mktplcExts = vscode-utils.extensionsFromVscodeMarketplace [
+        { publisher = "mjmorales"; name = "generic-lsp-proxy"; version = "2.1.2"; hash = "sha256-KvnGuf5Mjo/uCcXa7j2kt8Wx8vR72pziQridCVxEAKU="; }
       ];
     in
      nixExts ++ mktplcExts;
